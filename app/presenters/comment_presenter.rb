0929524327369328fe048cc375ccfe38fcfbdb66
Thomas Bruderer <apophis@apophis.ch>
2016-12-28 11:42:30 +0100

    image_tag resolves assets on their own

--- app/presenters/comment_presenter.rb
+++ app/presenters/comment_presenter.rb
@@ -2,7 +2,7 @@ class CommentPresenter < AbstractPresenter
   def comment_view
     h.content_tag(:div, class: 'comment') do
       h.link_to(m.user, class: 'avatar') do
-        h.image_tag(m.user ? m.user.avatar_url(:thumb) : asset_path('guest-avatar.png'));
+        h.image_tag(m.user ? m.user.avatar_url(:thumb) : 'guest-avatar.png');
       end + h.content_tag(:div, class: 'content') do
         (m.user ? h.link_to(m.user.name, m.user, class: 'author') : 'Unknown Guest'.html_safe) +
         h.content_tag(:div, class: 'metadata') do
